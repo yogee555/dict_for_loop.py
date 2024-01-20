c059cad159092be0b63b5b6d6f2ecb3cb8a80427
YOGEESH K P <148324344+yogee555@users.noreply.github.com>
2024-01-20 09:16:06 +0530

    Add files via upload

---
+++ dict_for_loop.py
@@ -0,0 +1,12 @@
+# dict use in python for forloop condition
+dict = {"1":"python","2":"java"}
+for i in dict:
+    print(i)
+
+dicts = {'6':'java','4':'script'}
+for i in dicts:
+    print(i)
+
+dictes = {'3':'html','5':'css'}
+for i in dictes:
+    print(i)
